2442f4b07634e5bfa8505fe562b64aea890fc78f
saintbrunswick <104053587+saintbrunswick@users.noreply.github.com>
2022-04-23 18:46:11 +0100

    1st day

---
+++ Python/simple_math.py
@@ -0,0 +1,17 @@
+num1 = input("Please type first number: ")
+num2 = input("Now, please type second number: ")
+num1 = int(num1)
+num2 = int(num2)
+
+addition = num1 + num2
+multi = num1 * num2
+minusAB = num1 - num2
+minusBA = num2 - num1
+
+print("If you add them together, you will get:" + str(addition))
+print("If you muliply them, you will get: " + str(multi))
+print("If you divine them you will get: " + str(minusAB))
+print("But if you divin them other way, you will get: " + str(minusBA))
+
+# Zglowy a wszystko dziala...
+# Jestem w szoku
