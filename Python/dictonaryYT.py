fa33546ad0caa999fca4b54f986d47dd30d6a452
saintbrunswick <104053587+saintbrunswick@users.noreply.github.com>
2022-04-24 17:09:31 +0100

    Python z telefonu

---
+++ Python/dictonaryYT.py
@@ -0,0 +1,20 @@
+dict1 = {
+    'name': 'xyz',
+    'age': 25,
+    'hobby': 'Dancing'
+}
+print(dict1['name'])
+
+dict1['name'] = 'abc'
+
+print(dict1['name'])
+
+dict1 = {
+    'status': 'Single',
+    'pet': 'Dog'
+}
+
+print(dict1['pet'])
+
+# del dict1   will remowe the entire dictonary
+#dict1.clear() will remove all items in dictonary
---
+++ Python/listAndTuple.py
@@ -0,0 +1,12 @@
+list1 = [2, 4, 5, 6, 7]
+
+list2 = [4, "hi", 6, "Me", 78]
+
+tuple1 = (2, 4, 5, 6, 7, 2)
+
+tuple2 = (4, "hi", 6, "Me", 78)
+
+print(tuple1[4])
+print(tuple2[3])
+print(list1[0], list2[0])
+print(tuple1[1:4])
