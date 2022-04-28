ca430874967a4cb2266b1f57abda4a77bebc09aa
saintbrunswick <104053587+saintbrunswick@users.noreply.github.com>
2022-04-28 13:29:10 +0100

    Basics

--- Python/basic2.py
+++ Python/basic2.py
@@ -10,9 +10,22 @@ print("Nice to meet you " + sname + " " + fname)
 # y = input("Your message")
 
 
+print("/n")
 # Accept a seqence of cmma-separated numbers from user and generate a list and a tuple with those numbers.
 values = input("Input some comma separated numbers: ")
 list = values.split(",")
 tuple = tuple(list)
-print("List : ",list)
-print("Tuple : ",tuple)
+print("List : ", list)
+print("Tuple : ", tuple)
+
+print("/n")
+# Accept a file name from user and print the extension of that.
+filename = input("Input the Filename: ")
+f_extens = filename.split(".")
+print("The extension of the file is: " + repr(f_extens[-1]))
+
+print("/n")
+# Program to display the first and last color from the fallowing list
+# color_list = ["Red","Green","White","Black"]
+color_list = ["Red", "Green", "White", "Black"]
+print("%s %s" % (color_list[0], color_list[-1]))
---
+++ Python/basic3.py
@@ -0,0 +1,40 @@
+#Program to display the examination schedule (extract the date from exam_sr_date)
+from datetime import date
+import calendar
+exam_st_date = (11, 12, 2022)
+print("The examination will start from: %i / %i / %i" % exam_st_date)
+
+print("/n")
+# Program that accepts integer(n) and compute the value of n+nn+nnn.
+a = int(input("Input an integer: "))
+n1 = int("%s" % (a))
+n2 = int("%s%s" % (a, a))
+n3 = int("%s%s%s" % (a, a, a))
+print(n1+n2+n3)
+
+print("/n")
+# Program to print the document(syntax, description etc.) of Python build in function(s).
+print(abs.__doc__)
+
+print("/n")
+# Program to print the calendar of a given month and year.
+y = int(input("Input the year: "))
+m = int(input("Input the month: "))
+print(calendar.month(y, m))
+
+print("/n")
+
+# Display formated text
+print("""
+a string that you "don't" have to escape
+This
+is a ......... multi-line
+heredoc string ------> example
+""")
+
+print("/n")
+# Program to calculate number fo days between two dates.
+f_date = date(2022, 4, 23)
+l_date = date(2022, 12, 24)
+delta = l_date - f_date
+print(delta.days)
